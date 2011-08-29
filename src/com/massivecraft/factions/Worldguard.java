445d10afb76823a678108803246d539044ebbced
Spathi <spathi@playminecraft.net>
2011-08-29 04:59:49 +0100

    Optional disabling of WG checks

--- src/com/massivecraft/factions/Worldguard.java
+++ src/com/massivecraft/factions/Worldguard.java
@@ -28,18 +28,18 @@ import org.bukkit.entity.Player;
 public class Worldguard {
 	private static WorldGuardPlugin wg;
 	private static boolean enabled = false;
-	
+
 	public static void init(Plugin plugin) {
 		Plugin wgplug = plugin.getServer().getPluginManager().getPlugin("WorldGuard");
 		if (wgplug == null || !(wgplug instanceof WorldGuardPlugin)) {
 			enabled = false;
 			wg = null;
 			System.out.println("[Factions] Could not hook to WorldGuard. WorldGuard checks are disabled.");
-	    } else {
-	    	wg = (WorldGuardPlugin) wgplug;
-	    	enabled = true;
-	    	System.out.println("[Factions] Successfully hooked to WorldGuard.");
-	    }
+		} else {
+			wg = (WorldGuardPlugin) wgplug;
+			enabled = true;
+			System.out.println("[Factions] Successfully hooked to WorldGuard.");
+		}
 	}
 
 	public static boolean isEnabled() {
@@ -64,46 +64,50 @@ public class Worldguard {
 			return true;
 		}
 	}
-	
+
 	// Check for Regions in chunk the chunk
 	// Returns:
 	//   True: Regions found within chunk
 	//   False: No regions found within chunk
 	public static boolean checkForRegionsInChunk(Location loc) {
-		int plocX = loc.getBlockX();
-		int plocZ = loc.getBlockZ();
-		World world = loc.getWorld();
-		
-		Chunk chunk = world.getChunkAt(plocX, plocZ);
-		int chunkX = chunk.getX();
-		int chunkZ = chunk.getZ();
-		
-		BlockVector minChunk = new BlockVector(chunkX, 0, chunkZ);
-		BlockVector maxChunk = new BlockVector(chunkX+15, 128, chunkZ+15);
-		
-		RegionManager regionManager = wg.getRegionManager(world);
-		ProtectedCuboidRegion region = new ProtectedCuboidRegion("wgfactionoverlapcheck", minChunk, maxChunk);
-		Map<String, ProtectedRegion> allregions = regionManager.getRegions(); 
-		
-		List<ProtectedRegion> allregionslist = new ArrayList<ProtectedRegion>(allregions.values());
-		List<ProtectedRegion> overlaps;
-		boolean foundregions = false;
+		if(isEnabled()) {
+			int plocX = loc.getBlockX();
+			int plocZ = loc.getBlockZ();
+			World world = loc.getWorld();
 
-		try {
-			overlaps = region.getIntersectingRegions(allregionslist);
-			if(overlaps.isEmpty() || overlaps == null) {
-				foundregions = false;
-			} else {
-				foundregions = true;
+			Chunk chunk = world.getChunkAt(plocX, plocZ);
+			int chunkX = chunk.getX();
+			int chunkZ = chunk.getZ();
+
+			BlockVector minChunk = new BlockVector(chunkX, 0, chunkZ);
+			BlockVector maxChunk = new BlockVector(chunkX+15, 128, chunkZ+15);
+
+			RegionManager regionManager = wg.getRegionManager(world);
+			ProtectedCuboidRegion region = new ProtectedCuboidRegion("wgfactionoverlapcheck", minChunk, maxChunk);
+			Map<String, ProtectedRegion> allregions = regionManager.getRegions(); 
+
+			List<ProtectedRegion> allregionslist = new ArrayList<ProtectedRegion>(allregions.values());
+			List<ProtectedRegion> overlaps;
+			boolean foundregions = false;
+
+			try {
+				overlaps = region.getIntersectingRegions(allregionslist);
+				if(overlaps.isEmpty() || overlaps == null) {
+					foundregions = false;
+				} else {
+					foundregions = true;
+				}
+			} catch (UnsupportedIntersectionException e) {
+				e.printStackTrace();
 			}
-		} catch (UnsupportedIntersectionException e) {
-			e.printStackTrace();
-		}
 
-		region = null;
-		allregionslist = null;
-		overlaps = null;
-		
-		return foundregions;
+			region = null;
+			allregionslist = null;
+			overlaps = null;
+
+			return foundregions;
+		} else {
+			return false;
+		}
 	}
 }
