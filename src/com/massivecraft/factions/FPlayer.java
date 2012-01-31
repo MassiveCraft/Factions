a1fcda003d45dc58ad20c1bbb93abcbc978b9fce
first4ever <hannibal_f4e@hotmail.fr>
2012-01-31 23:30:24 +0100

    Added a "claimedLandsMax" option in config (answering to issue "Max Faction Land" : https://github.com/MassiveCraft/Factions/issues/88 )

--- src/com/massivecraft/factions/FPlayer.java
+++ src/com/massivecraft/factions/FPlayer.java
@@ -580,6 +580,9 @@ public class FPlayer extends PlayerEntity implements EconomyParticipator
 		{
 			error = P.p.txt.parse("<b>You can't claim more land! You need more power!");
 		}
+		else if (ownedLand >= Conf.claimedLandsMax && Conf.claimedLandsMax != 0) {
+			error = P.p.txt.parse("<b>Limit reached. You can't claim more land!");
+		}
 		else if (currentFaction.getRelationTo(forFaction).isAtLeast(Rel.TRUCE) && ! currentFaction.isNone())
 		{
 			error = P.p.txt.parse("<b>You can't claim this land due to your relation with the current owner.");
