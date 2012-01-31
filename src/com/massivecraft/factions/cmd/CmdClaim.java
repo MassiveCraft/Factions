b41b3c8d2c870adf097f0c70e0ef970e84bbc231
Brettflan <dsr@fenrisoft.com>
2012-01-31 10:58:25 -0600

    Protection against radius claim attempts which might overload the server

--- src/com/massivecraft/factions/cmd/CmdClaim.java
+++ src/com/massivecraft/factions/cmd/CmdClaim.java
@@ -34,11 +34,24 @@ public class CmdClaim extends FCommand
 	{
 		// Read and validate input
 		Faction forFaction = this.argAsFaction(0, myFaction);
+
+		// just to cut the unauthorized off immediately instead of going on to do radius calculations
+		if (! fme.canClaimForFaction(forFaction))
+		{
+			msg("<b>You do not currently have permission to claim land for the faction "+forFaction.describeTo(fme) +"<b>.");
+			return;
+		}
+
 		double radius = this.argAsDouble(1, 1d);
 		radius -= 0.5;
 		if (radius <= 0)
 		{
 			msg("<b>That radius is to small.");
+			return;
+		}
+		else if (radius > 100)  // huge radius can crash server
+		{
+			msg("<b>That radius is overly large. Remember that the radius is in chunks (16x16 blocks), not individual blocks.");
 			return;
 		}
 
@@ -47,7 +60,7 @@ public class CmdClaim extends FCommand
 		p.log(flocs);
 		for (FLocation floc : flocs)
 		{
-			fme.attemptClaim(forFaction, new Location(floc.getWorld(), floc.getX()*16, 1, floc.getZ()*16), true);
+			fme.attemptClaim(forFaction, new Location(floc.getWorld(), floc.getX() << 4, 1, floc.getZ() << 4), true);
 		}
 	}
 	
