8e96a75b562d68e48210d111604ff351eb5a41df
Brettflan <dsr@fenrisoft.com>
2012-02-07 17:02:54 -0600

    Update our MCore event handlers to use the newer Bukkit event system

--- src/com/massivecraft/factions/zcore/MPluginSecretPlayerListener.java
+++ src/com/massivecraft/factions/zcore/MPluginSecretPlayerListener.java
@@ -1,9 +1,11 @@
 package com.massivecraft.factions.zcore;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPreLoginEvent;
 
 import com.massivecraft.factions.zcore.persist.EM;
@@ -11,7 +13,7 @@ import com.massivecraft.factions.zcore.persist.Entity;
 import com.massivecraft.factions.zcore.persist.EntityCollection;
 import com.massivecraft.factions.zcore.persist.PlayerEntityCollection;
 
-public class MPluginSecretPlayerListener extends PlayerListener
+public class MPluginSecretPlayerListener implements Listener
 {
 	private MPlugin p;
 	public MPluginSecretPlayerListener(MPlugin p)
@@ -19,7 +21,7 @@ public class MPluginSecretPlayerListener extends PlayerListener
 		this.p = p;
 	}
 	
-	@Override
+	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event)
 	{
 		if (event.isCancelled()) return;
@@ -31,7 +33,7 @@ public class MPluginSecretPlayerListener extends PlayerListener
 		}
 	}
 	
-	@Override
+	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerChat(PlayerChatEvent event)
 	{
 		if (event.isCancelled()) return;
@@ -43,7 +45,7 @@ public class MPluginSecretPlayerListener extends PlayerListener
 		}
 	}
 	
-	@Override
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerPreLogin(PlayerPreLoginEvent event)
 	{
 		for (EntityCollection<? extends Entity> ecoll : EM.class2Entities.values())
