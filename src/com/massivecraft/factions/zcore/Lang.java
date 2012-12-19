31faf605dc1d3f2a627ee0bf500576563c585dbc
Brettflan <dsr@fenrisoft.com>
2012-12-19 06:43:25 -0600

    Release 1.8.0
    and sneaking in a typo fix

--- src/com/massivecraft/factions/zcore/Lang.java
+++ src/com/massivecraft/factions/zcore/Lang.java
@@ -6,6 +6,6 @@ public class Lang
 	public static final String permDoThat = "do that";
 	
 	public static final String commandSenderMustBePlayer = "<b>This command can only be used by ingame players.";
-	public static final String commandToFewArgs = "<b>To few arguments. <i>Use like this:";
+	public static final String commandToFewArgs = "<b>Too few arguments. <i>Use like this:";
 	public static final String commandToManyArgs = "<b>Strange argument \"<p>%s<b>\". <i>Use the command like this:";
 }
