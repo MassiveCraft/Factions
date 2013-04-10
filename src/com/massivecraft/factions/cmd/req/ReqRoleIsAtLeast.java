828c149cb03372de5cf4c21e963a284cc4d239f2
Olof Larsson <olof@sylt.nu>
2013-04-10 13:33:17 +0200

    Adding a ReqRoleIsAtLeast for future use.

---
+++ src/com/massivecraft/factions/cmd/req/ReqRoleIsAtLeast.java
@@ -0,0 +1,49 @@
+package com.massivecraft.factions.cmd.req;
+
+import org.bukkit.command.CommandSender;
+
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayerColl;
+import com.massivecraft.factions.Rel;
+import com.massivecraft.mcore.cmd.MCommand;
+import com.massivecraft.mcore.cmd.req.ReqAbstract;
+import com.massivecraft.mcore.util.SenderUtil;
+import com.massivecraft.mcore.util.Txt;
+
+public class ReqRoleIsAtLeast extends ReqAbstract
+{
+	private static final long serialVersionUID = 1L;
+	
+	// -------------------------------------------- //
+	// FIELDS
+	// -------------------------------------------- //
+	
+	private final Rel rel;
+	public Rel getRel() { return this.rel; }
+	
+	// -------------------------------------------- //
+	// INSTANCE & CONSTRUCT
+	// -------------------------------------------- //
+	
+	public static ReqRoleIsAtLeast get(Rel rel) { return new ReqRoleIsAtLeast(rel); }
+	private ReqRoleIsAtLeast(Rel rel) { this.rel = rel; }
+	
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+	
+	@Override
+	public boolean apply(CommandSender sender, MCommand command)
+	{
+		
+		FPlayer fplayer = FPlayerColl.i.get(SenderUtil.getSenderId(sender));
+		return fplayer.getRole().isAtLeast(this.rel);
+	}
+	
+	@Override
+	public String createErrorMessage(CommandSender sender, MCommand command)
+	{
+		return Txt.parse("<b>You must be <h>%s <b>or higher to do this.", Txt.getNicedEnum(this.rel));
+	}
+	
+}
