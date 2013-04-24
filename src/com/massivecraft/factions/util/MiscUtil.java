aea6c38eaff8511b4cf32e17e5df5a5350b4a767
Olof Larsson <olof@sylt.nu>
2013-04-24 19:04:53 +0200

    Nah lets not create a built in badword list. Its not doable due to the scunthorpe problem.

--- src/com/massivecraft/factions/util/MiscUtil.java
+++ src/com/massivecraft/factions/util/MiscUtil.java
@@ -24,7 +24,6 @@ public class MiscUtil
 		return values;
 	}
 	
-	/// TODO create name whitelist!!?
 	public static HashSet<String> substanceChars = new HashSet<String>(Arrays.asList(new String []{
 	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", 
 	"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
