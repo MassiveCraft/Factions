eceb10c185150f60634d5959f0a149ca9b596cdd
Ihaveaporpoise <v3rglas@gmail.com>
2014-09-30 17:20:06 -0500

    Prevent players in peaceful factions from damaging with harmful potions. Fixes issue #50.

--- src/main/java/com/massivecraft/factions/listeners/FactionsEntityListener.java
+++ src/main/java/com/massivecraft/factions/listeners/FactionsEntityListener.java
@@ -205,6 +205,15 @@ public class FactionsEntityListener implements Listener {
             return;
         }
 
+        if(thrower instanceof Player){
+            Player player = (Player) thrower;
+            FPlayer fPlayer = FPlayers.i.get(player);
+            if(badjuju && fPlayer.getFaction().isPeaceful()){
+                event.setCancelled(true);
+                return;
+            }
+        }
+
         // scan through affected entities to make sure they're all valid targets
         Iterator<LivingEntity> iter = event.getAffectedEntities().iterator();
         while (iter.hasNext()) {
