04fd18f17acf48fb3f7f56d8fd1afab0aeb35839
drtshock <drtshock13@gmail.com>
2014-10-13 14:10:18 -0500

    Add tooltips and commands for f invite command as specified in issue #73.

--- src/main/java/com/massivecraft/factions/cmd/CmdInvite.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdInvite.java
@@ -3,6 +3,8 @@ package com.massivecraft.factions.cmd;
 import com.massivecraft.factions.Conf;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.struct.Permission;
+import mkremins.fanciful.FancyMessage;
+import org.bukkit.ChatColor;
 
 public class CmdInvite extends FCommand {
 
@@ -43,7 +45,17 @@ public class CmdInvite extends FCommand {
 
         myFaction.invite(you);
 
-        you.msg("%s<i> invited you to %s", fme.describeTo(you, true), myFaction.describeTo(you));
+        // Tooltips, colors, and commands only apply to the string immediately before it.
+        FancyMessage message = new FancyMessage(fme.describeTo(you, true))
+                                       .tooltip("Click to join!").command("f join " + myFaction.getTag())
+                                       .then(" has invited you to join ").color(ChatColor.YELLOW)
+                                       .tooltip("Click to join!").command("f join " + myFaction.getTag())
+                                       .then(myFaction.describeTo(you))
+                                       .tooltip("Click to join!").command("f join " + myFaction.getTag());
+
+        message.send(you.getPlayer());
+
+        //you.msg("%s<i> invited you to %s", fme.describeTo(you, true), myFaction.describeTo(you));
         myFaction.msg("%s<i> invited %s<i> to your faction.", fme.describeTo(myFaction, true), you.describeTo(myFaction));
     }
 
