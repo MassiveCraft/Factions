0a80b7932142969b603e7d4f8bcbcb5d9b33bc41
drtshock <drtshock13@gmail.com>
2015-01-05 09:58:32 -0600

    Check for relation when a player tries to create a destination portal in claimed land. Adds #204.

--- src/main/java/com/massivecraft/factions/listeners/FactionsEntityListener.java
+++ src/main/java/com/massivecraft/factions/listeners/FactionsEntityListener.java
@@ -6,6 +6,7 @@ import com.massivecraft.factions.struct.Relation;
 import com.massivecraft.factions.util.MiscUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.TravelAgent;
 import org.bukkit.block.Block;
 import org.bukkit.entity.*;
 import org.bukkit.entity.minecart.ExplosiveMinecart;
@@ -17,6 +18,7 @@ import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.hanging.HangingBreakEvent.RemoveCause;
 import org.bukkit.event.hanging.HangingPlaceEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
@@ -517,6 +519,34 @@ public class FactionsEntityListener implements Listener {
                         (faction.isNormal() && (faction.hasPlayersOnline() ? Conf.territoryBlockFireballs : Conf.territoryBlockFireballsWhenOffline)) ||
                         (faction.isWarZone() && Conf.warZoneBlockFireballs) ||
                         faction.isSafeZone()) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onTravel(PlayerPortalEvent event) {
+        if (!P.p.getConfig().getBoolean("portals.limit", false)) {
+            return; // Don't do anything if they don't want us to.
+        }
+
+        TravelAgent agent = event.getPortalTravelAgent();
+
+        // If they aren't able to find a portal, it'll try to create one.
+        if (event.useTravelAgent() && agent.getCanCreatePortal() && agent.findPortal(event.getTo()) == null) {
+            FLocation loc = new FLocation(event.getTo());
+            Faction faction = Board.getInstance().getFactionAt(loc);
+            if (faction.isNone()) {
+                return; // We don't care about wilderness.
+            } else if (!faction.isNormal() && !event.getPlayer().isOp()) {
+                // Don't let non ops make portals in safezone or warzone.
+                event.setCancelled(true);
+                return;
+            }
+
+            FPlayer fp = FPlayers.getInstance().getByPlayer(event.getPlayer());
+            String mininumRelation = P.p.getConfig().getString("portals.minimum-relation", "MEMBER"); // Defaults to Neutral if typed wrong.
+            if (!fp.getFaction().getRelationTo(faction).isAtLeast(Relation.fromString(mininumRelation))) {
                 event.setCancelled(true);
             }
         }
