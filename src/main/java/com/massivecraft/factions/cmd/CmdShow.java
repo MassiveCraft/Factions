7b522be2c7ec490bc429fd83f691f0eb4e4205a6
Evan Lindsay <evanlindsay@live.com>
2015-03-31 03:14:20 -0700

    Remove duplicate message for land/power/maxpower

--- src/main/java/com/massivecraft/factions/cmd/CmdShow.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdShow.java
@@ -58,7 +58,6 @@ public class CmdShow extends FCommand {
         String boost = (powerBoost == 0.0) ? "" : (powerBoost > 0.0 ? TL.COMMAND_SHOW_BONUS.toString() : TL.COMMAND_SHOW_PENALTY.toString() + powerBoost + ")");
 
         String raidable = faction.getLandRounded() > faction.getPowerRounded() ? TL.RAIDABLE_TRUE.toString() : TL.RAIDABLE_FALSE.toString();
-        msg(TL.COMMAND_SHOW_POWER, faction.getLandRounded(), faction.getPowerRounded(), faction.getPowerMaxRounded(), boost, raidable);
 
         List<FancyMessage> allies = new ArrayList<FancyMessage>();
         List<FancyMessage> enemies = new ArrayList<FancyMessage>();
