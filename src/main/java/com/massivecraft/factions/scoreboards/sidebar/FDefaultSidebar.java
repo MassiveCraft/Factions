76e6fd27650aded039505780788e9114b2563a68
drtshock <drtshock13@gmail.com>
2015-08-06 16:34:01 -0500

    Add factionless sidebar.

--- src/main/java/com/massivecraft/factions/scoreboards/sidebar/FDefaultSidebar.java
+++ src/main/java/com/massivecraft/factions/scoreboards/sidebar/FDefaultSidebar.java
@@ -4,6 +4,7 @@ import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.P;
 import com.massivecraft.factions.scoreboards.FSidebarProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -11,12 +12,25 @@ public class FDefaultSidebar extends FSidebarProvider {
 
     @Override
     public String getTitle(FPlayer fplayer) {
-        return replaceTags(fplayer, P.p.getConfig().getString("scoreboard.default-title", "i love drt"));
+        return replaceTags(fplayer, P.p.getConfig().getString("scoreboard.default-title", "{name}"));
     }
 
     @Override
     public List<String> getLines(FPlayer fplayer) {
-        List<String> lines = P.p.getConfig().getStringList("scoreboard.default");
+        if (fplayer.hasFaction()) {
+            return getOutput(fplayer, "scoreboard.default");
+        } else if (P.p.getConfig().getBoolean("scoreboard.factionless-enabled", false)) {
+            return getOutput(fplayer, "scoreboard.factionless");
+        }
+        return getOutput(fplayer, "scoreboard.default"); // no faction, factionless-board disabled
+    }
+
+    public List<String> getOutput(FPlayer fplayer, String list) {
+        List<String> lines = P.p.getConfig().getStringList(list);
+
+        if (lines == null || lines.isEmpty()) {
+            return new ArrayList<String>();
+        }
 
         ListIterator<String> it = lines.listIterator();
         while (it.hasNext()) {
