8926d0b4343d0587140a109815d7d71d3ec682b2
drtshock <drtshock13@gmail.com>
2015-08-06 16:09:07 -0500

    Don't allow % in faction descriptions. Was messing up string formatting. Fixes #490

--- src/main/java/com/massivecraft/factions/cmd/CmdDescription.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdDescription.java
@@ -33,7 +33,9 @@ public class CmdDescription extends FCommand {
             return;
         }
 
-        myFaction.setDescription(TextUtil.implode(args, " ").replaceAll("(&([a-f0-9]))", "& $2"));  // since "&" color tags seem to work even through plain old FPlayer.sendMessage() for some reason, we need to break those up
+        // since "&" color tags seem to work even through plain old FPlayer.sendMessage() for some reason, we need to break those up
+        // And replace all the % because it messes with string formatting and this is easy way around that.
+        myFaction.setDescription(TextUtil.implode(args, " ").replaceAll("%", "").replaceAll("(&([a-f0-9]))", "& $2"));
 
         if (!Conf.broadcastDescriptionChanges) {
             fme.msg(TL.COMMAND_DESCRIPTION_CHANGED, myFaction.describeTo(fme));
--- src/main/java/com/massivecraft/factions/cmd/CmdShow.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdShow.java
@@ -90,6 +90,9 @@ public class CmdShow extends FCommand {
                     // replaces all variables with no home TL
                     parsed = parsed.substring(0, parsed.indexOf("{ig}")) + TL.COMMAND_SHOW_NOHOME.toString();
                 }
+                if(parsed.contains("%")) {
+                    parsed = parsed.replaceAll("%", ""); // Just in case it got in there before we disallowed it.
+                }
                 msg(p.txt.parse(parsed));
             }
         }
