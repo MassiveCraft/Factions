fe68d57b23d20b6e33aa2b77d46031f6e9e82684
Olof Larsson <olof@sylt.nu>
2015-09-09 08:40:35 +0200

    Fix bug. Nothing spawning.

--- src/com/massivecraft/factions/engine/EngineMain.java
+++ src/com/massivecraft/factions/engine/EngineMain.java
@@ -1137,25 +1137,45 @@ public class EngineMain extends EngineAbstract
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	public void blockMonstersAndAnimals(CreatureSpawnEvent event)
 	{
-		// First check that it's a natural spawn ..
+		// If this is a natural spawn ..
 		if ( ! NATURAL_SPAWN_REASONS.contains(event.getSpawnReason())) return;
 		
-		// .. and if they can spawn here ..
+		// ... get the spawn location ...
 		Location location = event.getLocation();
 		if (location == null) return;		
-		
 		PS ps = PS.valueOf(location);
 		
+		// ... get the faction there ...
 		Faction faction = BoardColl.get().getFactionAt(ps);
 		if (faction == null) return;
 		
+		// ... get the entity type ...
 		EntityType type = event.getEntityType();
 		
-		if ((faction.getFlag(MFlag.getFlagMonsters()) && MConf.get().entityTypesMonsters.contains(type)) && 
-			(faction.getFlag(MFlag.getFlagAnimals()) && MConf.get().entityTypesAnimals.contains(type))) return;
-				
-		// ... block the spawn.
+		// ... and if this type can't spawn in the faction ...
+		if (canSpawn(faction, type)) return;
+		
+		// ... then cancel.
 		event.setCancelled(true);
+	}
+	
+	public static boolean canSpawn(Faction faction, EntityType type)
+	{
+		if (MConf.get().entityTypesMonsters.contains(type))
+		{
+			// Monster
+			return faction.getFlag(MFlag.getFlagMonsters());
+		}
+		else if (MConf.get().entityTypesAnimals.contains(type))
+		{
+			// Animal
+			return faction.getFlag(MFlag.getFlagAnimals());
+		}
+		else
+		{
+			// Other
+			return true;
+		}
 	}
 	
 	// -------------------------------------------- //
