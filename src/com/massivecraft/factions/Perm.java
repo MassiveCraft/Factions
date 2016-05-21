72c3270d204959c53e096126f95e6af2ae12efee
Olof Larsson <olof@sylt.nu>
2016-05-21 12:36:18 +0200

    MassiveCore - PermUtil --> PermissionUtil

--- src/com/massivecraft/factions/Perm.java
+++ src/com/massivecraft/factions/Perm.java
@@ -2,7 +2,7 @@ package com.massivecraft.factions;
 
 import org.bukkit.permissions.Permissible;
 
-import com.massivecraft.massivecore.util.PermUtil;
+import com.massivecraft.massivecore.util.PermissionUtil;
 
 public enum Perm
 {
@@ -110,7 +110,7 @@ public enum Perm
 	
 	public boolean has(Permissible permissible, boolean informSenderIfNot)
 	{
-		return PermUtil.has(permissible, this.node, informSenderIfNot);
+		return PermissionUtil.has(permissible, this.node, informSenderIfNot);
 	}
 	
 	public boolean has(Permissible permissible)
