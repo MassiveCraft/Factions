a7ed865c994cd5580c7a9ad7bffee78a4339a82d
Olof Larsson <olof@sylt.nu>
2016-05-26 11:47:11 +0200

    Remove Unused Import

--- src/com/massivecraft/factions/entity/MPlayer.java
+++ src/com/massivecraft/factions/entity/MPlayer.java
@@ -25,7 +25,6 @@ import com.massivecraft.factions.event.EventFactionsMembershipChange.MembershipC
 import com.massivecraft.factions.util.RelationUtil;
 import com.massivecraft.massivecore.mixin.MixinSenderPs;
 import com.massivecraft.massivecore.mixin.MixinTitle;
-import com.massivecraft.massivecore.nms.NmsChat;
 import com.massivecraft.massivecore.ps.PS;
 import com.massivecraft.massivecore.ps.PSFormatHumanSpace;
 import com.massivecraft.massivecore.store.SenderEntity;
