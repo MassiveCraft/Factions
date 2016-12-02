1fc3eb34296be4c5bf103a48dc3092775ef8c766
TheComputerGeek2 <Sam1010D@gmail.com>
2016-12-01 22:24:07 -0800

    Update default collections for 1.11 Also fixes comparators not being included as changing on interact

--- src/com/massivecraft/factions/entity/MConf.java
+++ src/com/massivecraft/factions/entity/MConf.java
@@ -536,7 +536,9 @@ public class MConf extends Entity<MConf>
 		"CAULDRON", // Minecraft 1.?
 		"SOIL", // Minecraft 1.?
 		"DAYLIGHT_DETECTOR", // Minecraft 1.5
-		"DAYLIGHT_DETECTOR_INVERTED" // Minecraft 1.5
+		"DAYLIGHT_DETECTOR_INVERTED", // Minecraft 1.5
+		"REDSTONE_COMPARATOR_OFF", // Minecraft 1.?
+		"REDSTONE_COMPARATOR_ON" // Minecraft 1.?
 	);
 	
 	// Interacting with the the terrain holding this item in hand results in an edit.
@@ -592,7 +594,25 @@ public class MConf extends Entity<MConf>
 		"BEACON", // Minecraft 1.?
 		"TRAPPED_CHEST", // Minecraft 1.?
 		"HOPPER", // Minecraft 1.?
-		"DROPPER" // Minecraft 1.?
+		"DROPPER", // Minecraft 1.?
+		
+		// The various shulker boxes, they had to make each one a different material -.-
+		"BLACK_SHULKER_BOX", // Minecraft 1.11
+		"BLUE_SHULKER_BOX", // Minecraft 1.11
+		"BROWN_SHULKER_BOX", // Minecraft 1.11
+		"CYAN_SHULKER_BOX", // Minecraft 1.11
+		"GRAY_SHULKER_BOX", // Minecraft 1.11
+		"GREEN_SHULKER_BOX", // Minecraft 1.11
+		"LIGHT_BLUE_SHULKER_BOX", // Minecraft 1.11
+		"LIME_SHULKER_BOX", // Minecraft 1.11
+		"MAGENTA_SHULKER_BOX", // Minecraft 1.11
+		"ORANGE_SHULKER_BOX", // Minecraft 1.11
+		"PINK_SHULKER_BOX", // Minecraft 1.11
+		"PURPLE_SHULKER_BOX", // Minecraft 1.11
+		"RED_SHULKER_BOX", // Minecraft 1.11
+		"SILVER_SHULKER_BOX", // Minecraft 1.11
+		"WHITE_SHULKER_BOX", // Minecraft 1.11
+		"YELLOW_SHULKER_BOX" // Minecraft 1.11
 	);
 	
 	// Interacting with these entities results in an edit.
@@ -619,22 +639,31 @@ public class MConf extends Entity<MConf>
 		"BLAZE", // Minecraft 1.?
 		"CAVE_SPIDER", // Minecraft 1.?
 		"CREEPER", // Minecraft 1.?
+		"ELDER_GUARDIAN", // minecraft 1.11
 		"ENDERMAN", // Minecraft 1.?
 		"ENDERMITE", // Minecraft 1.8
 		"ENDER_DRAGON", // Minecraft 1.?
+		"EVOKER", // Minecraft 1.11
 		"GUARDIAN", // Minecraft 1.8
 		"GHAST", // Minecraft 1.?
 		"GIANT", // Minecraft 1.?
+		"HUSK", // Minecraft 1.11
 		"MAGMA_CUBE", // Minecraft 1.?
 		"PIG_ZOMBIE", // Minecraft 1.?
 		"POLAR_BEAR", // Minecraft 1.10
 		"SILVERFISH", // Minecraft 1.?
+		"SHULKER", // Minecraft 1.10
 		"SKELETON", // Minecraft 1.?
 		"SLIME", // Minecraft 1.?
 		"SPIDER", // Minecraft 1.?
+		"STRAY", // Minecraft 1.11
+		"VINDICATOR", // Minecraft 1.11
+		"VEX", // Minecraft 1.11
 		"WITCH", // Minecraft 1.?
 		"WITHER", // Minecraft 1.?
-		"ZOMBIE" // Minecraft 1.?
+		"WITHER_SKELETON", // Minecraft 1.11
+		"ZOMBIE", // Minecraft 1.?
+		"ZOMBIE_VILLAGER" // Minecraft 1.11
 	);
 	
 	// List of entities considered to be animals.
@@ -642,14 +671,19 @@ public class MConf extends Entity<MConf>
 		"BAT", // Minecraft 1.?
 		"CHICKEN", // Minecraft 1.?
 		"COW", // Minecraft 1.?
+		"DONKEY", // Minecraft 1.11
 		"HORSE", // Minecraft 1.?
+		"LLAMA", // Minecraft 1.11
+		"MULE", // Minecraft 1.11
 		"MUSHROOM_COW", // Minecraft 1.?
 		"OCELOT", // Minecraft 1.?
 		"PIG", // Minecraft 1.?
 		"RABBIT", // Minecraft 1.?
 		"SHEEP", // Minecraft 1.?
+		"SKELETON_HORSE", // Minecraft 1.11
 		"SQUID", // Minecraft 1.?
-		"WOLF" // Minecraft 1.?
+		"WOLF", // Minecraft 1.?
+		"ZOMBIE_HORSE" // Minecraft 1.11
 	);
 	
 	// -------------------------------------------- //
