Nrc0611/Factions-2.11

Change PowerMixin to newer setup

src/com/massivecraft/factions/Factions.java
@@ -13,14 +13,14 @@ import com.massivecraft.factions.chat.modifier.ChatModifierParse;
 import com.massivecraft.factions.chat.modifier.ChatModifierRp;
 import com.massivecraft.factions.chat.modifier.ChatModifierUc;
 import com.massivecraft.factions.chat.modifier.ChatModifierUcf;
+import com.massivecraft.factions.chat.tag.ChatTagName;
+import com.massivecraft.factions.chat.tag.ChatTagNameforce;
 import com.massivecraft.factions.chat.tag.ChatTagRelcolor;
 import com.massivecraft.factions.chat.tag.ChatTagRole;
 import com.massivecraft.factions.chat.tag.ChatTagRoleprefix;
-import com.massivecraft.factions.chat.tag.ChatTagName;
-import com.massivecraft.factions.chat.tag.ChatTagNameforce;
 import com.massivecraft.factions.chat.tag.ChatTagRoleprefixforce;
 import com.massivecraft.factions.chat.tag.ChatTagTitle;
-import com.massivecraft.factions.cmd.*;
+import com.massivecraft.factions.cmd.CmdFactions;
 import com.massivecraft.factions.engine.EngineChat;
 import com.massivecraft.factions.engine.EngineCombat;
 import com.massivecraft.factions.engine.EngineEcon;
@@ -31,20 +31,19 @@ import com.massivecraft.factions.entity.Board;
 import com.massivecraft.factions.entity.BoardColl;
 import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.FactionColl;
+import com.massivecraft.factions.entity.MConfColl;
 import com.massivecraft.factions.entity.MFlagColl;
 import com.massivecraft.factions.entity.MPermColl;
 import com.massivecraft.factions.entity.MPlayerColl;
-import com.massivecraft.factions.entity.MConfColl;
 import com.massivecraft.factions.integration.V19.IntegrationV19;
 import com.massivecraft.factions.integration.herochat.IntegrationHerochat;
 import com.massivecraft.factions.integration.lwc.IntegrationLwc;
 import com.massivecraft.factions.integration.spigot.IntegrationSpigot;
 import com.massivecraft.factions.integration.worldguard.IntegrationWorldGuard;
 import com.massivecraft.factions.mixin.PowerMixin;
-import com.massivecraft.factions.mixin.PowerMixinDefault;
+import com.massivecraft.factions.task.TaskEconLandReward;
 import com.massivecraft.factions.task.TaskFlagPermCreate;
 import com.massivecraft.factions.task.TaskPlayerDataRemove;
-import com.massivecraft.factions.task.TaskEconLandReward;
 import com.massivecraft.factions.task.TaskPlayerPowerUpdate;
 import com.massivecraft.factions.update.UpdateUtil;
 import com.massivecraft.massivecore.Aspect;
@@ -100,9 +99,8 @@ public class Factions extends MassivePlugin
 	public boolean isDatabaseInitialized() { return this.databaseInitialized; }
 	
 	// Mixins
-	private PowerMixin powerMixin = null;
-	public PowerMixin getPowerMixin() { return this.powerMixin == null ? PowerMixinDefault.get() : this.powerMixin; }
-	public void setPowerMixin(PowerMixin powerMixin) { this.powerMixin = powerMixin; }
+	@Deprecated public PowerMixin getPowerMixin() { return PowerMixin.get(); }
+	@Deprecated public void setPowerMixin(PowerMixin powerMixin) { PowerMixin.get().setInstance(powerMixin); }
 	
 	// Gson without preprocessors
 	public final Gson gsonWithoutPreprocessors = this.getGsonBuilderWithoutPreprocessors().create();

MassiveCraft/Factions

src/com/massivecraft/factions/Factions.java
@@ -1,9 +1,5 @@
 package com.massivecraft.factions;
 
-import com.massivecraft.factions.cmd.type.TypeFactionChunkChangeType;
-import com.massivecraft.factions.cmd.type.TypeRel;
-import com.massivecraft.factions.event.EventFactionsChunkChangeType;
-import com.massivecraft.massivecore.command.type.RegistryType;
 import org.bukkit.ChatColor;
 
 import com.massivecraft.factions.adapter.BoardAdapter;
@@ -25,6 +21,8 @@ import com.massivecraft.factions.chat.tag.ChatTagRoleprefix;
 import com.massivecraft.factions.chat.tag.ChatTagRoleprefixforce;
 import com.massivecraft.factions.chat.tag.ChatTagTitle;
 import com.massivecraft.factions.cmd.CmdFactions;
+import com.massivecraft.factions.cmd.type.TypeFactionChunkChangeType;
+import com.massivecraft.factions.cmd.type.TypeRel;
 import com.massivecraft.factions.engine.EngineCanCombatHappen;
 import com.massivecraft.factions.engine.EngineChat;
 import com.massivecraft.factions.engine.EngineChunkChange;
@@ -55,13 +53,13 @@ import com.massivecraft.factions.entity.MConfColl;
 import com.massivecraft.factions.entity.MFlagColl;
 import com.massivecraft.factions.entity.MPermColl;
 import com.massivecraft.factions.entity.MPlayerColl;
+import com.massivecraft.factions.event.EventFactionsChunkChangeType;
 import com.massivecraft.factions.integration.V19.IntegrationV19;
 import com.massivecraft.factions.integration.herochat.IntegrationHerochat;
 import com.massivecraft.factions.integration.lwc.IntegrationLwc;
 import com.massivecraft.factions.integration.spigot.IntegrationSpigot;
 import com.massivecraft.factions.integration.worldguard.IntegrationWorldGuard;
 import com.massivecraft.factions.mixin.PowerMixin;
-import com.massivecraft.factions.mixin.PowerMixinDefault;
 import com.massivecraft.factions.task.TaskEconLandReward;
 import com.massivecraft.factions.task.TaskFlagPermCreate;
 import com.massivecraft.factions.task.TaskPlayerDataRemove;
@@ -71,6 +69,7 @@ import com.massivecraft.massivecore.Aspect;
 import com.massivecraft.massivecore.AspectColl;
 import com.massivecraft.massivecore.MassivePlugin;
 import com.massivecraft.massivecore.Multiverse;
+import com.massivecraft.massivecore.command.type.RegistryType;
 import com.massivecraft.massivecore.util.MUtil;
 import com.massivecraft.massivecore.xlib.gson.Gson;
 import com.massivecraft.massivecore.xlib.gson.GsonBuilder;
@@ -120,9 +119,8 @@ public class Factions extends MassivePlugin
 	public boolean isDatabaseInitialized() { return this.databaseInitialized; }
 	
 	// Mixins
-	private PowerMixin powerMixin = null;
-	public PowerMixin getPowerMixin() { return this.powerMixin == null ? PowerMixinDefault.get() : this.powerMixin; }
-	public void setPowerMixin(PowerMixin powerMixin) { this.powerMixin = powerMixin; }
+	@Deprecated public PowerMixin getPowerMixin() { return PowerMixin.get(); }
+	@Deprecated public void setPowerMixin(PowerMixin powerMixin) { PowerMixin.get().setInstance(powerMixin); }
 	
 	// Gson without preprocessors
 	public final Gson gsonWithoutPreprocessors = this.getGsonBuilderWithoutPreprocessors().create();